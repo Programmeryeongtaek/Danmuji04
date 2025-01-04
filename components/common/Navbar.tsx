2c0e687db20a928b8474c5795228ae9784f286c7
YeongTaek <yeongtaek2109@gmail.com>
2025-01-04 17:46:23 +0900

    style: update background styling

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="fixed bottom-0 flex h-[60px] w-full items-center bg-light">
+    <nav className="fixed bottom-0 flex h-[60px] w-full items-center">
       <ul className="flex w-full justify-between px-8">
         <li>임시1</li>
         <li>임시2</li>
