e259f49634e69c99da477cd225340077b9a077d5
YeongTaek <yeongtaek2109@gmail.com>
2025-01-06 02:37:44 +0900

    feat: Sort Card Content by Category Selection

--- components/knowledge/LectureSection.tsx
+++ components/knowledge/LectureSection.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Card from '../common/Card';
 import KeywordSelector from './KeywordSelector';
 import Dropdown from '../common/Dropdown';
@@ -6,7 +8,25 @@ import Filter from './Filter';
 import { lectures } from '@/dummy/lectureData';
 import { LectureSectionProps } from '@/types/knowledge/lecture';
 
+const categoryLabelMap = new Map([
+  ['all', '전체'],
+  ['search', '검색'],
+  ['humanities', '인문학'],
+  ['philosophy', '철학'],
+  ['psychology', '심리학'],
+  ['economics', '경제학'],
+  ['self-development', '자기계발'],
+  ['leadership', '리더십'],
+]);
+
 const LectureSection = ({ selectedCategory }: LectureSectionProps) => {
+  const filteredLectures = lectures.filter((lecture) => {
+    if (selectedCategory === 'all') return true;
+    if (selectedCategory === 'search') return false;
+
+    return lecture.keyword === categoryLabelMap.get(selectedCategory);
+  });
+
   return (
     <div className="flex flex-col">
       <div className="flex justify-between">
