239ae8a4ed26b6e78e5f959d5ad96ee1192ec476
YeongTaek <yeongtaek2109@gmail.com>
2025-01-06 17:40:22 +0900

    style: modify Button's property

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({
 }: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
-      className={`w-full rounded-lg bg-gradient-to-r from-gold-start to-gold-end text-white transition-all duration-300 hover:bg-gradient-to-l hover:from-gold-start hover:to-gold-end disabled:cursor-not-allowed disabled:opacity-50 ${className} `}
+      className={`rounded-lg bg-gradient-to-r from-gold-start to-gold-end text-white transition-all duration-300 hover:bg-gradient-to-l hover:from-gold-start hover:to-gold-end disabled:cursor-not-allowed disabled:opacity-50 ${className} `}
       {...props}
     >
       {children}
