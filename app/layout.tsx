e570b36fe93cacf040f40159cf97315373533d05
YeongTaek <yeongtaek2109@gmail.com>
2025-01-16 05:58:55 +0900

    feat: isolate signup page from main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,34 @@
+'use client';
+
 import './globals.css';
 import Header from '@/components/common/Header';
 import Navbar from '@/components/common/Navbar';
+import { Inter } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
+const inter = Inter({ subsets: ['latin'] });
+
 const RootLayout = ({ children }: RootLayoutProps) => {
+  const pathname = usePathname();
+  const isSignUpPage = pathname === '/signup';
+
+  if (isSignUpPage) {
+    return (
+      <html lang="ko">
+        <body className={inter.className}>{children}</body>
+      </html>
+    );
+  }
+
   return (
     <html lang="ko">
-      <body className="h-full bg-light">
-        <div>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
           <Header />
           <main>{children}</main>
           <Navbar />
