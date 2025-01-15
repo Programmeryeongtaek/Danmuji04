4e9fee5d291e7b5302b9f975e29baa539c8ab1e1
YeongTaek <yeongtaek2109@gmail.com>
2025-01-16 03:46:54 +0900

    feat: create nevItems array and implement navigation with Link component

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,13 +1,36 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: '대시보드', path: '/dashboard' },
+    { name: '지식', path: '/knowledge' },
+    { name: '홈', path: '/' },
+    { name: '코스', path: '/course' },
+    { name: '더보기', path: '/more' },
+  ];
+
   return (
     <nav className="fixed bottom-0 flex h-[60px] w-full items-center">
-      <ul className="flex w-full justify-between px-8">
-        <li>임시1</li>
-        <li>임시2</li>
-        <li>임시3</li>
-        <li>임시4</li>
-        <li>임시5</li>
-      </ul>
+      <div className="flex w-full">
+        {navItems.map((item) => (
+          <Link key={item.path} href={item.path} className="h-full flex-1">
+            <button
+              className={`flex h-full w-full items-center justify-center ${
+                pathname === item.path
+                  ? 'border-gold-start text-gold-start'
+                  : 'border-transparent text-light-subtext'
+              }`}
+            >
+              {item.name}
+            </button>
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 };
